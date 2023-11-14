ec44bc4c32627de30ed77fddc22b1d2f311ec3b8
Sarita Gajmer <saritagajmer36@gmail.com>
2023-11-14 14:47:32 -0500

    capstone

--- scripts/mountains.js
+++ scripts/mountains.js
@@ -1,3 +1,19 @@
 "use strict";
 
-console.log(mountainsArray);
+// console.log(mountainsArray);
+
+//get html elements
+const mountainsList = document.getElementById("mountainsList");
+
+//create functions
+function loadMountainsList() {
+  for (const mountainName of mountainsArray) {
+    let option = document.createElement("option");
+    option.textContent = mountainName;
+    option.value = mountainName;
+    mountainsList.appendChild(option);
+  }
+}
+
+//wire-up functions
+loadMountainsList();
--- scripts/parks.js
+++ scripts/parks.js
@@ -30,5 +30,56 @@ function loadNationalParkTable() {
     }
   }
 }
+function loadNationalParkTableByStateName(stateName) {
+  
+  for (const park of nationalParksArray) {
+    if (park.State == stateName) {
+      let row = nationalParkTableBody.insertRow(-1);
+      let cell1 = row.insertCell(0);
+      cell1.innerText = park.LocationName;
+      let cell2 = row.insertCell(1);
+      cell2.innerText = park.Address;
+      let cell3 = row.insertCell(2);
+      cell3.innerText = park.City;
+      let cell4 = row.insertCell(3);
+      cell4.innerText = park.State;
+      let cell5 = row.insertCell(4);
+      cell5.innerText = park.ZipCode;
+      let cell6 = row.insertCell(5);
+      cell6.innerText = park.Phone;
+      if (park.Visit) {
+        var link = document.createElement("a");
+        let cell7 = row.insertCell(6);
+        link.href = park.Visit;
+        link.innerText = park.LocationName;
+        cell7.appendChild(link);
+      }
+      console.log(park);
+    }
+  }
+}
+
 //wire-up function to events that was created in the last step
-loadNationalParkTable();
+// loadNationalParkTable();
+
+//get html elements into variables
+const statesList = document.getElementById("statesList");
+
+//write functions
+function loadStatesList() {
+  for (const stateName of locationsArray) {
+    let option = document.createElement("option");
+    option.textContent = stateName;
+    option.value = stateName;
+    statesList.appendChild(option);
+  }
+}
+function statesListChanged() {
+  const stateName = statesList.value;
+  console.log(stateName);
+  loadNationalParkTableByStateName(stateName);
+}
+
+//wire-up functions that was created in the last step
+loadStatesList();
+statesList.onchange = loadNationalParkTableByStateName;
